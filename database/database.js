b1e646f2c422eb30ef3c534ac54a01f20db40049
abdullah-alkhareji <outsiderandco@gmail.com>
2022-02-16 18:56:48 +0300

    add abdullah's conniction

--- database/database.js
+++ database/database.js
@@ -5,7 +5,8 @@ dotenv.config();
 const connectDB = async () => {
   const DATABASE = "foodDB";
   const PASSWORD = process.env.PASSWORD;
-  const CONNECTION_URL = `mongodb+srv://adim:${PASSWORD}@coded.j2ddu.mongodb.net/${DATABASE}?retryWrites=true&w=majority`;
+  // const CONNECTION_URL = `mongodb+srv://adim:${PASSWORD}@coded.j2ddu.mongodb.net/${DATABASE}?retryWrites=true&w=majority`;
+  const CONNECTION_URL = `mongodb+srv://admin:${PASSWORD}@coded.9yoca.mongodb.net/${DATABASE}?retryWrites=true&w=majority`;
   console.log(CONNECTION_URL);
   const conn = await mongoose.connect(CONNECTION_URL, {
     useUnifiedTopology: true,
